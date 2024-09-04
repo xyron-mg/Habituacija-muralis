831351dd6a0988bc5596d50abeb603dba5c5f3e5
xyron-mg <mglogoski@gmail.com>
2024-09-04 11:08:56 +0200

    testing add

--- Scripts/github.R
+++ Scripts/github.R
@@ -6,6 +6,7 @@ use_git_config(user.name = "xyron-mg", user.email = "mglogoski@gmail.com")
 usethis::git_default_branch_configure()
 usethis::create_github_token()
 
-install.packages("gitcreds")
 library(gitcreds)
 gitcreds::gitcreds_set()
+
+#testing add
