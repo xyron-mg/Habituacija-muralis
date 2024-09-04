7df7ee8199483263080d17fb10e520be3e82f258
xyron-mg <mglogoski@gmail.com>
2024-09-04 14:16:14 +0200

    test

--- Scripts/github.R
+++ Scripts/github.R
@@ -12,3 +12,9 @@ gitcreds_get()
 #testing add
 
 usethis::git_remotes()
+
+library(usethis)
+use_git()
+use_git_config(user.name = "xyron-mg", user.email = "mglogoski@gmail.com")
+usethis::git_default_branch_configure()
+
